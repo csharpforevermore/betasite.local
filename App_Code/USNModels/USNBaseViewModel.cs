2cd1b55389e968926359096f03e05c929ce3becc
Chris Randle <csharpforevermore@gmail.com>
2019-11-28 12:58:10 +0000

    Migrating template

---
+++ App_Code/USNModels/USNBaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace USN.USNModels
+{
+    /// <summary>
+    /// Not using strongly typed models here so that PureLive mode can be used
+    /// </summary>
+    public class USNBaseViewModel : ContentModel
+    {
+        //Standard Model Pass Through
+        public USNBaseViewModel(IPublishedContent content) : base(content) { }
+
+        public IPublishedContent GlobalSettings { get; set; }
+
+        public IPublishedContent Navigation { get; set; }
+    }
+}
