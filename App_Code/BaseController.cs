c8e28cf87f9ce6f017ccf3a6804191a238d98c35
Chris Randle <csharpforevermore@gmail.com>
2019-11-29 16:55:49 +0000

    Added more base classes

---
+++ App_Code/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using ESO.Models;
+
+namespace ESO.ESOControllers
+{
+    /// <summary>
+    /// Not using strongly typed models here so that PureLive mode can be used
+    /// </summary>
+    public class BaseController : Umbraco.Web.Mvc.RenderMvcController
+    {
+        public override ActionResult Index(ContentModel model)
+        {
+            // IPublishedContent homeNode = model.Content.Site();
+            IPublishedContent homeNode = Umbraco.ContentAtRoot().FirstOrDefault();
+
+            if (homeNode.IsDocumentType("Homepage"))
+            {
+                IPublishedContent globalSettings = homeNode.Value<IPublishedContent>("websiteConfigurationNode").Children.Where(x => x.IsDocumentType("GlobalSettings")).First();
+                IPublishedContent navigation = homeNode.Value<IPublishedContent>("websiteConfigurationNode").Children.Where(x => x.IsDocumentType("Navigation")).First();
+
+                BaseViewModel customModel = new BaseViewModel(model.Content);
+                customModel.GlobalSettings = globalSettings;
+                customModel.Navigation = navigation;
+
+                return base.Index(customModel);
+            }
+            else
+                return base.Index(model);
+        }
+    }
+}
